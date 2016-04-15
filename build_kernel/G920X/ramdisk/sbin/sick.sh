1182684fdd152ef4614db61a87dbe0d4969659bf
The-Sickness <alloycowboy6969@gmail.com>
2016-04-15 18:44:26 -0500

    Google play services wakelock fix

---
+++ build_kernel/G920X/ramdisk/sbin/sick.sh
@@ -0,0 +1,31 @@
+#!/system/bin/sh
+
+/sbin/busybox mount -t rootfs -o remount,rw rootfs
+
+sync
+
+/system/xbin/daemonsu --auto-daemon &
+
+# Make internal storage directory.
+if [ ! -d /data/.sick ]; then
+	mkdir /data/.sick
+fi;
+
+# Init.d
+busybox run-parts /system/etc/init.d
+
+/sbin/busybox mount -t rootfs -o remount,ro rootfs
+/sbin/busybox mount -o remount,rw /data
+
+# Google play services wakelock fix
+sleep 40
+su -c "pm enable com.google.android.gms/.update.SystemUpdateActivity"
+su -c "pm enable com.google.android.gms/.update.SystemUpdateService"
+su -c "pm enable com.google.android.gms/.update.SystemUpdateService$ActiveReceiver"
+su -c "pm enable com.google.android.gms/.update.SystemUpdateService$Receiver"
+su -c "pm enable com.google.android.gms/.update.SystemUpdateService$SecretCodeReceiver"
+su -c "pm enable com.google.android.gsf/.update.SystemUpdateActivity"
+su -c "pm enable com.google.android.gsf/.update.SystemUpdatePanoActivity"
+su -c "pm enable com.google.android.gsf/.update.SystemUpdateService"
+su -c "pm enable com.google.android.gsf/.update.SystemUpdateService$Receiver"
+su -c "pm enable com.google.android.gsf/.update.SystemUpdateService$SecretCodeReceiver"
